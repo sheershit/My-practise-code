a94446eceb43eaff4b4a957e2e5a9ab3abd70b3f
Sheersh Bansal <141630396+sheershit@users.noreply.github.com>
2023-10-27 22:20:18 +0530

    Update no in fabonacii series or not.c

--- no in fabonacii series or not.c
+++ no in fabonacii series or not.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+int main() {
+    int a,b,c,n;
+    
+    printf("Enter the number");
+    scanf("%d",&n);
+    a=1,b=-1;
+    for(int i=0;  ;i++)
+    {
+        c=a+b;
+        
+        if(c==n)
+        {
+            printf("number in series");
+            break;
+        }
+        
+        if(c>n) {
+        printf("number is not in series");
+        break; // loop sebahr aane ki hai
+        
+        }
+        a=b;
+        b=c;
+        
+    }
+    return 0;
+    
+}
 
