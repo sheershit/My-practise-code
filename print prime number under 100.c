5c82a2046c6276586b3f0511dda0a76b099136ca
Sheersh Bansal <141630396+sheershit@users.noreply.github.com>
2023-10-31 21:22:03 +0530

    Create print prime number under 100.c

---
+++ print prime number under 100.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+    
+    int i,j;
+    
+    for(i=2;i<=100;i++)
+    {
+        for(j=2;j<=i-1;j++)
+        if(i%j==0)
+        break;
+
+    if(i==j)
+    printf("%d\n",j);
+    }
+    return 0;
+}
